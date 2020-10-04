c477d1017b7e958592ac92887860bcfff02da0f6
Fernando <biagettifernando@gmail.com>
2020-10-04 00:01:08 -0300

    Delete unsage modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,15 +9,13 @@ import { LoginComponent } from './components/login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AlertsComponent } from './components/shared/alerts/alerts.component';
 import { SpinnerComponent } from './components/shared/spinner/spinner.component';
-import { ModalComponent } from './components/shared/modal/modal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     AlertsComponent,
-    SpinnerComponent,
-    ModalComponent
+    SpinnerComponent
   ],
   imports: [
     BrowserModule,
